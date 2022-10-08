53fe392496ce182f6545fa6b25a30fd187946e8f
yuma-y4 <mfnvbbflbgrt5tupni6nijtfpxroff@gmail.com>
2022-10-08 18:32:50 +0900

    fix login_error done#5

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
-  get "/signup", to: "users#new"
-  get "/users/sign_out" => "devise/sessions#destroy"
+  devise_for :users do
+    get "sessions/new"
+    get "/users/sign_out" => "devise/sessions#destroy"
+  end
   root "check_pages#home"
+  get "/login",   to: "sessions#new"
+  get "/signup", to: "users#new"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,8 @@
 #
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  config.include Devise::Test::IntegrationHelpers, type: :request # sign_inヘルパーを提供
+  config.include FactoryBot::Syntax::Methods # FactoryBot
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
